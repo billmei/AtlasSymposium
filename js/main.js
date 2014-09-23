1e70e181a1f74712ab2deeec960967adfd88059e
Bill Mei <billmei@users.noreply.github.com>
2014-09-23 16:19:24 -0400

    Update date/time/location

--- js/main.js
+++ js/main.js
@@ -3,20 +3,6 @@ $('#email-submit').on('mouseover', function() {
     $('#email-submit').val('Send me an email reminder');
 });
 
-$('#facebookNav').on('mouseenter', function(){
-    $('#facebookText').show(200);
-    $(this).on('mouseleave', function (){
-        $('#facebookText').hide(50);
-    });
-});
-
-$('#twitterNav').on('mouseenter', function(){
-    $('#twitterText').show(200);
-    $(this).on('mouseleave', function (){
-        $('#twitterText').hide(50);
-    });
-});
-
 // Tooltips for abbreviations
 $('#SAF').tooltip({"placement":"bottom"});
 $('#TRSM').tooltip({"placement":"bottom"});
